d2e7c6d2ed2a55e3895375858704202f75b8a2f5
Luktony <luktony7@yahoo.com.br>
2023-09-15 19:16:41 -0300

    changes index.js and settings gitignore

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import  express, { text }  from "express"
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
 import { summarize } from "./summarize.js"
+import {convert} from "./convert.js"
 
 const app = express()
 app.use (express.json())
@@ -12,20 +13,35 @@ app.use (express.json())
 app.use(cors())
 
  app.get('/summary/:id', async (request, response) =>{
+    try{
+
     await download(request.params.id)
+    const audioConverted=await convert()
+ 
     
-    const result = await transcribe()
+    const result = await transcribe(audioConverted)
     
     
     return response.json({result:result})
+    } catch(error){
+        console.log(error)
+        return response.json({error})
+
+    }
+    
 })
 
-app.post("/summary", async (request, response ) => 
-{
+app.post("/summary", async (request, response ) => {
 
+try{
  const result =  await summarize(request.body.text)
 
 return response.json({result})
+}  catch(error){
+    console.log(error)
+    return response.json({error})
+
+}
 })
 
 
